1e99098af6f7183f2c09c1103cb13c108fb414b2
nembot <adwaith1998kv@gmail.com>
2024-09-14 13:00:44 +0100

    Modified engine builder script for trtv10

--- convert_onnx_to_trt.py
+++ convert_onnx_to_trt.py
@@ -49,12 +49,6 @@ def parse_args():
         default="FP32",
         help="Precision mode used in converted model.",
     )
-    parser.add_argument(
-        "--max_workspace_size",
-        type=int,
-        default=2048,
-        help="Max workspace size in MiB.",
-    )
 
     args, _ = parser.parse_known_args()
     return args
@@ -77,74 +71,38 @@ def create_profile_with_shapes(builder: trt.Builder):
     # )
     return profile
 
-
-def prepare_config(
-    args: Namespace, config: trt.IBuilderConfig
-) -> trt.IBuilderConfig:
-    config.max_workspace_size = args.max_workspace_size * (1024**2)
-    if args.precision_mode == "FP16":
-        config.set_flag(trt.BuilderFlag.FP16)
-    config.builder_optimization_level = args.optimization_level
-    return config
-
-
-def build_engine(
-    builder: trt.Builder,
-    network: trt.INetworkDefinition,
-    config: trt.IBuilderConfig,
-) -> trt.IHostMemory:
-    print("Building engine. This might take a while .........")
-    start_time = time.time()
-    serialized_engine = builder.build_serialized_network(network, config)
-    if serialized_engine is None:
-        raise ValueError("Building TensorRT Engine failed.")
-    end_time = time.time()
-    duration = end_time - start_time
-    print(f"Engine built, time taken: {duration:.2f}s.")
-    return serialized_engine
-
-
 def build_tensorrt_network(
     args: Namespace,
     trt_logger: trt.Logger,
-    explicit_batch: int,
 ):
-    with trt.Builder(
-        trt_logger
-    ) as builder, builder.create_builder_config() as config:
-        profile = create_profile_with_shapes(builder)
-        config.add_optimization_profile(profile)
+    builder = trt.Builder(trt_logger,)
+    config = builder.create_builder_config()
+    profile=create_profile_with_shapes(builder)
+    config.add_optimization_profile(profile)
+    network = builder.create_network()
+    parser = trt.OnnxParser(network, trt_logger)
+    success = parser.parse_from_file(args.model_onnx_path)
 
-        with builder.create_network(explicit_batch) as network, trt.OnnxParser(
-            network, trt_logger
-        ) as parser:
-            with open(args.model_onnx_path, "rb") as model:
-                if not parser.parse(model.read()):
-                    for error in range(parser.num_errors):
-                        print(parser.get_error(error))
+    if not success:
+        for idx in range(parser.num_errors):
+            print(parser.get_error(idx))
+    if args.precision_mode == "FP16":
+        config.set_flag(trt.BuilderFlag.FP16)
 
-            print(
-                "INFO: Batch size is set to 1. If this needs to be changed, "
-                "code needs to be upadted."
-            )
-            batch_size = 1
-            builder.max_batch_size = batch_size
-
-            config = prepare_config(args, config)
-            serialized_engine = build_engine(builder, network, config)
-
-            with open(args.model_trt_path, "wb") as output_file:
-                output_file.write(serialized_engine)
-
-
+    config.builder_optimization_level=args.optimization_level
+    print("Building engine. This might take a while .........")
+    start_time = time.time()
+    serialized_engine = builder.build_serialized_network(network, config)
+    end_time = time.time()
+    duration = end_time - start_time
+    print(f"Engine built, time taken: {duration:.2f}s.")
+    with open(args.model_trt_path, "wb") as output_file:
+        output_file.write(serialized_engine)
 def main():
     args = parse_args()
     trt_logger = LOGGER_DICT["verbose"]
-    explicit_batch = 1 << (int)(
-        trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH
-    )
+    build_tensorrt_network(args, trt_logger,)
 
-    build_tensorrt_network(args, trt_logger, explicit_batch)
 
 
 if __name__ == "__main__":
